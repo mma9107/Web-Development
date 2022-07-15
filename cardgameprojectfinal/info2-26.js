c3e0e91c99cf03d01479e43614c8371fa0b5bafc
mma9107 <45180807+mma9107@users.noreply.github.com>
2022-07-14 22:17:37 -0400

    Uploaded functional version of card game
    Final version of WAR card game played on webpage

---
+++ cardgameprojectfinal/info2-26.js
@@ -0,0 +1,9 @@
+SOURCE = 0, SUIT = 1, VALUE = 2;
+
+info = "1-0.png:clubs:13;1-1.png:diamonds:13;1-2.png:hearts:13;1-3.png:spades:13;2-0.png:clubs:1;2-1.png:diamonds:1;2-2.png:hearts:1;2-3.png:spades:1;3-0.png:clubs:2;3-1.png:diamonds:2;3-2.png:hearts:2;3-3.png:spades:2;4-0.png:clubs:3;4-1.png:diamonds:3;4-2.png:hearts:3;4-3.png:spades:3;5-0.png:clubs:4;5-1.png:diamonds:4;5-2.png:hearts:4;5-3.png:spades:4;6-0.png:clubs:5;6-1.png:diamonds:5;6-2.png:hearts:5;6-3.png:spades:5;7-0.png:clubs:6;7-1.png:diamonds:6;7-2.png:hearts:6;7-3.png:spades:6;8-0.png:clubs:7;8-1.png:diamonds:7;8-2.png:hearts:7;8-3.png:spades:7;9-0.png:clubs:8;9-1.png:diamonds:8;9-2.png:hearts:8;9-3.png:spades:8;10-0.png:clubs:9;10-1.png:diamonds:9;10-2.png:hearts:9;10-3.png:spades:9;11-0.png:clubs:10;11-1.png:diamonds:10;11-2.png:hearts:10;11-3.png:spades:10;12-0.png:clubs:11;12-1.png:diamonds:11;12-2.png:hearts:11;12-3.png:spades:11;13-0.png:clubs:12;13-1.png:diamonds:12;13-2.png:hearts:12;13-3.png:spades:12";
+
+/*
+Suits = [clovers, diamonds, hearts, spades]
+Names = [Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King]
+}
+*/
